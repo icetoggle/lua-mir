42f549319e93104ac232e6db0ff80afb0b0c4d4b
icetoggle <l2c@qq.com>
2022-09-17 16:58:55 +0800

    add mir_context to LUA_REGISTRYINDEX and add gc

--- c2luafun.c
+++ c2luafun.c
@@ -32,10 +32,46 @@ static int get_data(void *data)
     return code->code[code->cur_idx++];
 }
 
+typedef struct {
+    MIR_context_t ctx;
+}mir_ctx_wrapper;
+
+static int mir_context_gc(lua_State *L)
+{
+    mir_ctx_wrapper *ctx_wrapper = (mir_ctx_wrapper *)lua_touserdata(L, 1);
+    MIR_finish(ctx_wrapper->ctx);
+    return 0;
+}
+
+static MIR_context_t init_mir_context(lua_State *L)
+{
+    lua_pushstring(L, "__mir_context");
+    lua_gettable(L, LUA_REGISTRYINDEX);
+    if (lua_isnil(L, -1)) {
+        lua_pop(L, 1);
+        MIR_context_t ctx = MIR_init();
+        lua_pushstring(L, "__mir_context");
+        mir_ctx_wrapper *wrapper = lua_newuserdata(L, sizeof(mir_ctx_wrapper));
+        wrapper->ctx = ctx;
+        luaL_newmetatable(L, "mir_context");
+        lua_pushstring(L, "__gc");
+        lua_pushcfunction(L, mir_context_gc);
+        lua_settable(L, -3);
+        lua_setmetatable(L, -2);
+        lua_settable(L, LUA_REGISTRYINDEX);
+        return ctx;
+    } else {
+        MIR_context_t ctx = lua_touserdata(L, -1);
+        lua_pop(L, 1);
+        return ctx;
+    }
+    return NULL;
+}
+
 
 lua_CFunction create_lua_func_from_c(lua_State *L, const char *source_name, const char* code)
 {
-    MIR_context_t ctx = MIR_init();
+    MIR_context_t ctx = init_mir_context(L);
     MIR_gen_init(ctx, 1);
     c2mir_init(ctx);
     struct c2mir_options options;
@@ -75,6 +111,7 @@ lua_CFunction create_lua_func_from_c(lua_State *L, const char *source_name, cons
     }
     MIR_link(ctx, MIR_set_gen_interface, import_luacfun_resolver);
     MIR_gen(ctx, 0, rt_func);
+    MIR_gen_finish(ctx);
     lua_CFunction lua_func = (lua_CFunction)rt_func->addr;
     return lua_func;
 }
